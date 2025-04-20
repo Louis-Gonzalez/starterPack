1f378e722e3f03ab8ad1d2816ed9fde2efde53f0
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-04-20 14:13:25 +0200

    ADD svg multi-colors without img tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Home from './../assets/icons/home.svg'
+import Microsoft from './../assets/icons/microsoft.svg'
 const colorSvg = ['deeppink', 'darkblue', 'darkcyan', 'darkgreen', 'darkorange', 'olive', 'crimson', 'chartreuse', 'cornFlowerBlue', 'gold','magenta']
 
 const currentColor = ref(colorSvg[0])
@@ -12,29 +13,44 @@ const changeColor = () => {
 </script>
 
 <template>
-  <h1>{{$t('home')}}</h1>
-  <div>
-    <h2>Welcome ! This is the home page</h2>
+  <section>
+    <h1>{{ $t('home') }}</h1>
     <div>
+      <h2>Welcome! This is the home page</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quaerat.
         Exercitationem dolorem quo aliquam ratione quia. Harum doloribus atque
         assumenda. Quos aspernatur laudantium quibusdam. Placeat expedita
         excepturi quis id quidem?
       </p>
-      <p class="toggle-vsg-change">
-        <Home :fill="currentColor" class="home-svg"/>
-        <v-btn @click="changeColor">Random Colors</v-btn>
-      </p>
+
+      <div class="d-flex justify-lg-space-around align-center ma-3">
+        <div class="text-center">
+          <Home :fill="currentColor" class="home-svg" />
+          <v-btn @click="changeColor" class="mt-2">Random Colors</v-btn>
+        </div>
+
+        <div class="text-left">
+          <article class="mb-2">
+            <strong>This is the Microsoft SVG!</strong><br />
+            No need to import it as an <code>&lt;img&gt;</code> thanks to the
+            <code>vite-svg-loader</code> dependency.
+          </article>
+          <Microsoft />
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
+
 
 <style scoped>
 .home-svg {
   transition: fill 0.3s ease;
-  width: 4rem;
-  height: 4rem;
 }
 
+svg {
+  width:4rem;
+  height: 4rem;
+}
 </style>
