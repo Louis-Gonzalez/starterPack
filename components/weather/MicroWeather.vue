f2739f4ef541fb41fd766f262e18e91ff125e4da
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-09-07 13:29:47 +0200

    UDT interface microWeather

--- components/weather/MicroWeather.vue
+++ components/weather/MicroWeather.vue
@@ -11,13 +11,18 @@
 <template>
   <v-card border class="mt-2 ma-2 pa-4 mini-card">
     <div class="d-flex align-center ma-2 pa-2">
-      <img v-if="props.item.srcIcon" :src="props.item.srcIcon" :alt="props.item.alt">
+      <img v-if="props.item.srcIcon" :src="props.item.srcIcon" :alt="props.item.alt" class="svg-class">
       <div class="ma-2 pa-2">
-        <p>{{ props.item.itemName}} :</p>
+        <p><strong>{{ props.item.itemName}} :</strong></p>
         <p>{{ props.item.itemValue }} {{itemUnit}}</p>
       </div>
     </div>
   </v-card>
 </template>
 
-<style scoped></style>
+<style scoped>
+ .svg-class {
+   width : 1.5rem;
+   height: 1.5rem;
+ }
+</style>
