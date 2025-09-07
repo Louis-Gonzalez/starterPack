38f8c3599cfa2f75555d54d3913039456081fac4
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-09-07 15:27:33 +0200

    UDT microweather

--- components/weather/MicroWeather.vue
+++ components/weather/MicroWeather.vue
@@ -1,28 +1,49 @@
 <script setup lang="ts">
-  import type { IMicroWeather } from '~/types';
+import type { IMicroWeather } from '~/types';
+import { computed } from 'vue';
 
-  const props = defineProps<{
-    item: IMicroWeather
-  }>()
+const props = defineProps<{
+  item: IMicroWeather
+}>()
 
-  const itemUnit = computed(() => props.item.itemUnit ?? "")
+const itemUnit = computed(() => props.item.itemUnit ?? "")
 </script>
 
 <template>
-  <v-card border class="mt-2 ma-2 pa-4 mini-card">
-    <div class="d-flex align-center ma-2 pa-2">
-      <img v-if="props.item.srcIcon" :src="props.item.srcIcon" :alt="props.item.alt" class="svg-class">
-      <div class="ma-2 pa-2">
-        <p><strong>{{ props.item.itemName}} :</strong></p>
-        <p>{{ props.item.itemValue }} {{itemUnit}}</p>
+  <div class="mt-2 ma-2 pa-2 mini-card">
+    <div class="d-flex align-center">
+      <!-- Icône à gauche -->
+      <img v-if="props.item.srcIcon" :src="props.item.srcIcon" :alt="props.item.alt" class="svg-class mr-2">
+
+      <!-- Texte à droite -->
+      <div class="d-flex flex-column justify-center">
+        <p class="item-name"><strong>{{ props.item.itemName }}</strong></p>
+        <p class="item-value">{{ props.item.itemValue }} {{ itemUnit }}</p>
       </div>
     </div>
-  </v-card>
+  </div>
 </template>
 
 <style scoped>
- .svg-class {
-   width : 1.5rem;
-   height: 1.5rem;
- }
+.svg-class {
+  width: 2rem;
+  height: 2rem;
+}
+
+.mini-card {
+  width: 8rem;
+  height: auto;
+  padding: 0.5rem;
+}
+
+.item-name {
+  margin: 0;
+  font-size: 0.9rem;
+}
+
+.item-value {
+  margin: 0;
+  font-size: 0.85rem;
+  color: #555;
+}
 </style>
