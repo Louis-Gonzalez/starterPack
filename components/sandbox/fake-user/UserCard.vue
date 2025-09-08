f29d0bdfeb0f33d52cf382920c8eb5da2142a52c
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-09-08 20:27:22 +0200

    ADD 2 components

---
+++ components/sandbox/fake-user/UserCard.vue
@@ -0,0 +1,12 @@
+
+<script setup lang="ts">
+  const props = defineProps<{ user: { name: string, email: string } }>()
+</script>
+
+<template>
+  <div class="border p-4 rounded">
+    <h3>{{props.user.name }}</h3>
+    <p>{{ props.user.email }}</p>
+  </div>
+</template>
+
---
+++ components/sandbox/fake-user/UserPicture.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const props = defineProps<{ user: { name: string, picture: string } }>()
+</script>
+
+<template>
+  <img :src="props.user.picture" :alt="props.user.name" class="rounded-full img-class" />
+</template>
+
+<style scoped>
+.img-class {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+</style>
+
+
