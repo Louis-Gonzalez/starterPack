63b73e6109900888fb5fcfccbc4232217b45dcb7
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-09-13 13:55:04 +0200

    ADD translate keys

--- pages/composables.vue
+++ pages/composables.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useI18n} from 'vue-i18n';
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 /// counter composable
 // const { count, increment, decrement, reset } = useCounter(5);
 
@@ -11,37 +11,37 @@ const email = ref<string>('');
 
 const handleSubmit = () => {
   console.log(lastname.value, lastname.value, email.value);
-}
+};
 </script>
 
 <template>
-<!--  <section id="counter-composable">-->
-<!--    <div class="p-4">-->
-<!--      <h1>Counter Section</h1>-->
-<!--      <p>Current value : {{ count }}</p>-->
+  <!--  <section id="counter-composable">-->
+  <!--    <div class="p-4">-->
+  <!--      <h1>Counter Section</h1>-->
+  <!--      <p>Current value : {{ count }}</p>-->
 
-<!--      <div class="d-flex align-center ma-1 pa-2 ga-2">-->
-<!--        <v-btn @click="increment">➕ 1</v-btn>-->
-<!--        <v-btn @click="decrement">➖ 1</v-btn>-->
-<!--        <v-btn @click="reset">🔄 Reset</v-btn>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </section>-->
+  <!--      <div class="d-flex align-center ma-1 pa-2 ga-2">-->
+  <!--        <v-btn @click="increment">➕ 1</v-btn>-->
+  <!--        <v-btn @click="decrement">➖ 1</v-btn>-->
+  <!--        <v-btn @click="reset">🔄 Reset</v-btn>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--  </section>-->
 
-<!--  :rules="['required','notEmpty','string','longerThan6']"-->
+  <!--  :rules="['required','notEmpty','string','longerThan6']"-->
 
-<!--  :rules="['required','notEmpty','string','longerThan6']"-->
+  <!--  :rules="['required','notEmpty','string','longerThan6']"-->
 
-<!--  :rules="['required','notEmpty','string','longerThan6', 'email']"-->
+  <!--  :rules="['required','notEmpty','string','longerThan6', 'email']"-->
   <section>
     <v-form class="ma-4" @prevent.default="handleSubmit">
+      <h1 class="text-center">{{ form_user_title }}</h1>
       <v-card border elevation="2" class="ma-2">
         <v-row class="d-flex ma-2 align-center">
           <v-col cols="4">
             <v-text-field
               v-model="lastname"
               :label="t('lastname')"
-
               variant="outlined"
             />
           </v-col>
@@ -49,7 +49,6 @@ const handleSubmit = () => {
             <v-text-field
               v-model="firstname"
               :label="t('firstname')"
-
               variant="outlined"
             />
           </v-col>
@@ -57,15 +56,16 @@ const handleSubmit = () => {
             <v-text-field
               v-model="email"
               :label="t('email')"
-
               variant="outlined"
             />
           </v-col>
         </v-row>
         <v-row>
           <v-col class="d-flex align-center ga-2 justify-center mb-4">
-            <v-btn color="error" outlined>Cancel</v-btn>
-            <v-btn color="success" type="submit" outlined >Confirm</v-btn>
+            <v-btn color="error" outlined>{{ t('cancel') }}</v-btn>
+            <v-btn color="success" type="submit" outlined>{{
+              t('confirm')
+            }}</v-btn>
           </v-col>
         </v-row>
       </v-card>
