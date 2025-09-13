210bba29f881aa450723ce87d605d5ba71d52ce6
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-09-13 11:44:15 +0200

    ADD use composable counter

--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,9 @@ const changeColor = () => {
   const randomIndex = Math.floor(Math.random() * colorSvg.length);
   currentColor.value = colorSvg[randomIndex];
 };
+
+/// counter composable
+const { count, increment, decrement, reset } = useCounter(5)
 </script>
 
 <template>
@@ -51,6 +54,19 @@ const changeColor = () => {
         </div>
       </div>
     </div>
+  </section>
+  <div class="p-4">
+    <h1>Counter Section</h1>
+    <p>Valeur actuelle : {{ count }}</p>
+
+    <div class="d-flex align-center ma-1 pa-2 ga-2">
+      <v-btn @click="increment">➕ 1</v-btn>
+      <v-btn @click="decrement">➖ 1</v-btn>
+      <v-btn @click="reset">🔄 Reset</v-btn>
+    </div>
+  </div>
+  <section id="counter-composable">
+
   </section>
 </template>
 
