9d3ca6547e27e20a2f810eb0aac798e8f7486f42
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-09-13 11:52:32 +0200

    UDT vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
@@ -11,4 +18,4 @@ export default defineConfig({
       reporter: ['text', 'json', 'html'],
     },
   },
-});
+})
