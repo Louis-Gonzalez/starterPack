a1f50595ad2dbca880883f6e29c583f177afc7ce
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-09-13 13:48:56 +0200

    UDT form

--- pages/composables.vue
+++ pages/composables.vue
@@ -8,6 +8,10 @@ const { t } = useI18n();
 const lastname = ref<string>('');
 const firstname = ref<string>('');
 const email = ref<string>('');
+
+const handleSubmit = () => {
+  console.log(lastname.value, lastname.value, email.value);
+}
 </script>
 
 <template>
@@ -23,32 +27,48 @@ const email = ref<string>('');
 <!--      </div>-->
 <!--    </div>-->
 <!--  </section>-->
-  <section>
-    <v-form class="ma-4">
-      <v-row class="d-flex ma-2 align-center">
-        <v-col cols="4">
-          <v-text-field
-            v-model="lastname"
-            :label="t('lastname')"
-            variant="outlined"
-          />
-        </v-col>
-        <v-col cols="4">
-          <v-text-field
-            v-model="firstname"
-            :label="t('firstname')"
-            variant="outlined"
-          />
-        </v-col>
-        <v-col cols="4">
-          <v-text-field
-            v-model="email"
-            :label="t('email')"
-            variant="outlined"
-          />
-        </v-col>
-      </v-row>
 
+<!--  :rules="['required','notEmpty','string','longerThan6']"-->
+
+<!--  :rules="['required','notEmpty','string','longerThan6']"-->
+
+<!--  :rules="['required','notEmpty','string','longerThan6', 'email']"-->
+  <section>
+    <v-form class="ma-4" @prevent.default="handleSubmit">
+      <v-card border elevation="2" class="ma-2">
+        <v-row class="d-flex ma-2 align-center">
+          <v-col cols="4">
+            <v-text-field
+              v-model="lastname"
+              :label="t('lastname')"
+
+              variant="outlined"
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="firstname"
+              :label="t('firstname')"
+
+              variant="outlined"
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="email"
+              :label="t('email')"
+
+              variant="outlined"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="d-flex align-center ga-2 justify-center mb-4">
+            <v-btn color="error" outlined>Cancel</v-btn>
+            <v-btn color="success" type="submit" outlined >Confirm</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-form>
   </section>
 </template>
