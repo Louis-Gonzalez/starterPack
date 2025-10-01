c10d9a5e4b681e9dc2ac296194ea779cc1a74791
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-10-01 21:44:47 +0200

    ADD new it with trigger

--- tests/units/homepage.test.js
+++ tests/units/homepage.test.js
@@ -1,4 +1,5 @@
 import { mount } from '@vue/test-utils'
+import { nextTick } from 'vue'
 import IndexPage from '~/pages/index.vue'
 
 describe('Homepage', () => {
@@ -13,8 +14,24 @@ describe('Homepage', () => {
 
     it('affiche le titre et les bouton', () => {
         const wrapper = mount(IndexPage)
-        const button = wrapper.findComponent('v-btn')
+        const button = wrapper.findComponent({ name: 'v-btn' })
         expect(button.exists()).toBe(true)
         expect(button.text()).toContain('Click Me')
+    })
+
+    it('affiche le texte après clic sur le bouton', async () => {
+        const wrapper = mount(IndexPage)
+
+        expect(wrapper.find('p').exists()).toBe(false)
+
+        const button = wrapper.findComponent({ name: 'v-btn' })
+        expect(button.exists()).toBe(true)
+
+        await button.trigger('click')
+        await nextTick()
+
+        const paragraph = wrapper.find('p')
+        expect(paragraph.exists()).toBe(true)
+        expect(paragraph.text()).toBe('This is a text ! or not !')
     })
 })
