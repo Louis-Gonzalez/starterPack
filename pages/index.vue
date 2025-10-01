0afbf392641268f430e89a2ddb506346e5f32a25
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-10-01 21:48:01 +0200

    UDT index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,26 @@
 <script setup>
 import { ref } from 'vue'
+
 const pageTitle = ref('Homepage')
+const isTextVisible = ref(false)
+
+const toggleText = () => {
+  isTextVisible.value = !isTextVisible.value
+}
 </script>
 
 <template>
   <h1>{{ pageTitle }}</h1>
+
   <v-btn
     color="primary"
     class="px-2"
     outlined
-    >
+    @click="toggleText"
+  >
     Click Me
   </v-btn>
-</template>
 
+  <p v-if="isTextVisible">This is a text !  or not ! </p>
+</template>
 
